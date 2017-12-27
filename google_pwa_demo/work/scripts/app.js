6b75d3de2f0fb4a52d20b0afe45a087b2f8031f3
行旅書讀 <huabinwen@gmail.com>
2017-12-27 10:04:15 +0800

    add service worker register

--- google_pwa_demo/work/scripts/app.js
+++ google_pwa_demo/work/scripts/app.js
@@ -348,4 +348,9 @@
 	
 
     // TODO add service worker code here
+	if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+  	           .register('./service-worker.js')
+  	           .then(function() { console.log('Service Worker Registered'); });
+  	}
 })();
