1c7ae7cf114e601b65c0e28d5e2622513053d52d
huabinwen <huabinwen@gmail.com>
2018-01-28 23:22:08 +0800

    add service worker listener

---
+++ src/service-worker.js
@@ -0,0 +1,13 @@
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  	console.log('[Service Worker] Fetch', e.request.url);
+});
