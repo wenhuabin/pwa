3a44a8bfcc59edabd5600f3785f7bbf4e3519bd4
行旅書讀 <huabinwen@gmail.com>
2018-01-29 09:15:48 +0800

    add install process

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,14 @@
+var cacheName = 'xlsdPWA';
+var filesToCache = [];
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            console.log('[SW] Cacheing app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 self.addEventListener('activate', function(e) {
