cd05d43905f68b505a18a440a1a1e8a18f850cac
行旅書讀 <huabinwen@gmail.com>
2018-01-30 09:45:39 +0800

    add activate and fetch

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,8 +1,9 @@
 var cacheName = 'xlsdPWA';
+var dataCacheName = 'xlsdPWA-v1';
 var filesToCache = [];
 
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+    console.log('[SW] Install');
     e.waitUntil(
         caches.open(cacheName).then(function(cache){
             console.log('[SW] Cacheing app shell');
@@ -12,10 +13,31 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  return self.clients.claim();
+    console.log('[SW] Activate');
+    caches.keys().then(function(keyList){
+        return Promise.all(keyList.map(function(key){
+            console.log('[SW] Remove old cache');
+            return caches.delete(key);
+        }
+    })):
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
-  	console.log('[Service Worker] Fetch', e.request.url);
+  	console.log('[SW] Fetch', e.request.url);
+    var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+    if(e.request.url.indexOf(dataUrl) > -1){
+        e.respondWith(
+            caches.open(dataCacheName).then(function(cache){
+                return fetch(e.request).then(function(response){
+                    return response;
+                });
+            });
+    }else{
+        e.respondWith(
+            cache.match(e.request).then(function(response){
+                return response || fetch(e.request);
+            });
+        );
+    }
 });
